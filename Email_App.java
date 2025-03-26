127eb2bb74f5304337c3b2732945041584af6cc4
rocktimH <136682574+rocktimH@users.noreply.github.com>
2025-03-26 20:06:51 +0530

    Add files via upload

---
+++ Email_App.java
@@ -0,0 +1,15 @@
+import EmailApp.Email;
+
+public class Email_App {
+    
+    public static void main(String[] args){
+
+        Email Em1 = new Email("Rocktim","Hazarika");
+
+        System.out.println(Em1.showinfo());
+        
+
+
+        
+    }
+}
